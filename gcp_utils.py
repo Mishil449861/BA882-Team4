9759bdfebf7e6f85c76d88aa4d8a1f63dab9fe24
Mishil Trivedi <trivedimishil31@gmail.com>
2025-10-20 18:00:16 -0400

    Update gcp_utils.py

--- gcp_utils.py
+++ gcp_utils.py
@@ -4,16 +4,10 @@ from google.cloud import bigquery
 from google.oauth2 import service_account
 
 def get_bq_client():
-    # Streamlit Secrets 환경 변수에서 서비스 계정 키 가져오기
-    key_json_str = os.environ["GCP_SERVICE_ACCOUNT"]
+    key_json_str = st.secrets["GCP_SERVICE_ACCOUNT"]
     key_info = json.loads(key_json_str)
-
-    # 서비스 계정 정보를 기반으로 인증 객체 생성
     credentials = service_account.Credentials.from_service_account_info(key_info)
-    project_id = key_info["project_id"]
-
-    # BigQuery 클라이언트 생성
-    return bigquery.Client(project=project_id, credentials=credentials)
+    return bigquery.Client(credentials=credentials, project=key_info["project_id"])
 
 
 # from google.cloud import bigquery
